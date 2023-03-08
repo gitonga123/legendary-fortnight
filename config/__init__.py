f00ffb6bd9efc9a9df016c3ace2b91c073d89bb3
Daniel Mutwiri <mutwiridanielsci@gmail.com>
2023-03-08 08:36:52 +0300

    Do away with python 3.7 action test, make config director to a python module

---
+++ config/__init__.py
@@ -0,0 +1,5 @@
+###################################################################################################
+# All configuration settings file under this folder, we will have both test and prod env files here
+###################################################################################################
+
+from .settings import *
