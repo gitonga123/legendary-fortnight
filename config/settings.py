cf49d506b1c6dc41840e1344f64c4819d02fa8ec
Daniel Mutwiri <mutwiridanielsci@gmail.com>
2023-03-08 08:30:00 +0300

    Restructure the files

--- config/settings.py
+++ config/settings.py
@@ -34,9 +34,19 @@ environ.Env.read_env(BASE_DIR / ".env")
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool("DEBUG", default=True)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    h.strip()
+    for h in env.str("ALLOWED_HOSTS", default="").split(",")
+    if h and h.strip()
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    h.strip()
+    for h in env.str("CSRF_TRUSTED_ORIGINS", default="").split(",")
+    if h and h.strip()
+]
 
 
 # Application definition
@@ -116,7 +126,16 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = env.str("TIME_ZONE", default="Africa/Nairobi")
+
+# Ensure that the csrf cookie is only sent under an HTTPS connection.
+CSRF_COOKIE_SECURE = env.bool("SSL_ENABLED", default=False)
+
+# Ensure that the session cookie is only sent under an HTTPS connection.
+SESSION_COOKIE_SECURE = env.bool("SSL_ENABLED", default=False)
+
+# Specify the duration of a given session
+SESSION_EXPIRE_SECONDS = env.int("SESSION_EXPIRE_SECONDS", default=3600)
 
 USE_I18N = True
 
