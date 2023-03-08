ca1c017396bb5eb6b3bf76cd0c0456dcf4853e1b
Daniel Mutwiri <mutwiridanielsci@gmail.com>
2023-03-08 08:44:53 +0300

    Remove config folder

--- config/__init__.py
+++
@@ -1,5 +0,0 @@
-###################################################################################################
-# All configuration settings file under this folder, we will have both test and prod env files here
-###################################################################################################
-
-from ..bank_statements.settings import *
