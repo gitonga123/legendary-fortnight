abd5bd3a27bae7dd71007d7ea4094ee6e934e801
Daniel Mutwiri <mutwiridanielsci@gmail.com>
2023-11-06 08:40:56 +0300

    Template and urls initialization

--- bank_statements/settings.py
+++ bank_statements/settings.py
@@ -14,6 +14,8 @@ import environ
 
 from pathlib import Path
 
+import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -58,6 +60,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core.apps.CoreConfig'
 ]
 
 MIDDLEWARE = [
@@ -146,6 +149,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = '/static/'
+
+STATICFILES_DIRS = (  
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
